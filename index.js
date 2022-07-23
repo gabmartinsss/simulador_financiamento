75ead79f6dfb2e743198722d283535141a403264
Gabrielle Martins <gabi_maartins@hotmail.com>
2022-07-23 01:19:10 -0300

    Calculation of installments and interest / calculation button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Financiamento } from "./financiamento.js";
+
 const comCarencia = document.querySelector('#comCarencia');
 const listaSuspensa = document.querySelector('#listaSuspensa');
 const corpoTabela = document.querySelector('#corpoTabela');
@@ -7,10 +9,21 @@ const textoEntrada = document.querySelector('#textoEntrada');
 const textoTaxaJuros = document.querySelector('#textoTaxaJuros');
 const textoPrazo = document.querySelector('#textoPrazo');
 
- comCarencia.addEventListener('change', function() {
+comCarencia.addEventListener('change', function() {
     if(this.checked){
         listaSuspensa.removeAttribute('hidden');
     } else {
         listaSuspensa.setAttribute('hidden','hidden');
     }
- });
+});
+
+botaoCalcular.addEventListener('click', function(){
+    const valor = parseFloat(textoValor.value);
+    const entrada = parseFloat(textoEntrada.value);
+    const taxaJuros = parseFloat(textoTaxaJuros.value);
+    const prazo = parseFloat(textoPrazo.value);
+    let simulacao;
+    simulacao = new Financiamento(valor, entrada, taxaJuros, prazo);
+    simulacao.calcParcelasMensais();
+    simulacao.exibeParcelas();
+});
